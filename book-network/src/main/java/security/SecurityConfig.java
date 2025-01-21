ceaf189c37263246ba5f0312d9d71bb8084d733c
Zied <zied.kannou15@gmail.com>
2025-01-21 23:05:15 +0100

    Back-end | Security continuation [3]

--- book-network/src/main/java/security/SecurityConfig.java
+++ book-network/src/main/java/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package security;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
