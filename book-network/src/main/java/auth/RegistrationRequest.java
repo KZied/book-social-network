9504f5b79e2e3f4a923f1f6dffac034b593a5b36
Zied <zied.kannou15@gmail.com>
2025-01-22 21:50:08 +0100

    Back-end | Authentication controller init

---
+++ book-network/src/main/java/auth/RegistrationRequest.java
@@ -0,0 +1,35 @@
+package auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    @NotEmpty(message = "First name is mandatory")
+    @NotBlank(message = "First name is mandatory")
+    private String firstname;
+    @NotEmpty(message = "Last name is mandatory")
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+    @Email(message = "Email format is not valid")
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @NotEmpty(message = "Password is mandatory")
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Password must be 8 characters long at least")
+    private String password;
+
+
+}
