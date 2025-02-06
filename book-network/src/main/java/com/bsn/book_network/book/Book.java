8a719f453ad4db149b7a182c8e2672c1f6ca48c2
Zied <zied.kannou15@gmail.com>
2025-02-06 22:17:13 +0100

    Back-end | BookController: save, findById and findAllbooks

--- book-network/src/main/java/com/bsn/book_network/book/Book.java
+++ book-network/src/main/java/com/bsn/book_network/book/Book.java
@@ -4,20 +4,16 @@ import com.bsn.book_network.common.BaseEntity;
 import com.bsn.book_network.feedback.Feedback;
 import com.bsn.book_network.history.BookTransactionHistory;
 import com.bsn.book_network.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book extends BaseEntity {
@@ -45,6 +41,19 @@ public class Book extends BaseEntity {
 
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> histories;
+
+    @Transient
+    public double getRate() {
+        if(feedbacks == null || feedbacks.isEmpty()) {
+            return 0.0;
+        }
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        double roundedRate = Math.round(rate * 10.0) / 10.0;
+        return roundedRate;
+    }
 
 
 }
