614ed2abfb69ce2dab3645f1952a16343d2b4d1f
Zied <zied.kannou15@gmail.com>
2025-02-10 20:18:16 +0100

    Back-end | findAllFeedbacksByBook

--- book-network/src/main/java/com/bsn/book_network/feedback/FeedbackController.java
+++ book-network/src/main/java/com/bsn/book_network/feedback/FeedbackController.java
@@ -1,14 +1,12 @@
 package com.bsn.book_network.feedback;
 
+import com.bsn.book_network.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("feedbacks")
@@ -23,6 +21,15 @@ public class FeedbackController {
             @Valid @RequestBody FeedbackRequest request,
             Authentication connectedUser) {
         return ResponseEntity.ok(service.saveFeedback(request, connectedUser));
+    }
 
+    @GetMapping("/book/{book-id}")
+    public ResponseEntity<PageResponse<FeedbackResponse>> findAllFeedbacksByBook(
+            @PathVariable("book-id") Integer bookId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllFeedbacksByBook(bookId, page, size, connectedUser));
     }
 }
--- book-network/src/main/java/com/bsn/book_network/feedback/FeedbackService.java
+++ book-network/src/main/java/com/bsn/book_network/feedback/FeedbackService.java
@@ -2,12 +2,17 @@ package com.bsn.book_network.feedback;
 
 import com.bsn.book_network.book.Book;
 import com.bsn.book_network.book.BookRepository;
+import com.bsn.book_network.common.PageResponse;
 import com.bsn.book_network.exception.OperationNotPermittedException;
 import com.bsn.book_network.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -29,5 +34,23 @@ public class FeedbackService {
         }
         Feedback feedback = feedbackMapper.toFeedback(request);
         return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+        Pageable pageable = PageRequest.of(page, size);
+        User user = (User) connectedUser.getPrincipal();
+        Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
     }
 }
