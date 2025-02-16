76246b1dfb3b62b41f9a0d64ff3cd7cfd83a823a
Zied <zied.kannou15@gmail.com>
2025-02-16 20:53:29 +0100

    Front-end | Login functionality finalized

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -11,11 +14,36 @@ export class LoginComponent {
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMsgs: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: TokenService
+  ) {
+  }
+
   login() {
+    this.errorMsgs = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+        this.tokenService.token = res.token as string;
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if(err.error.validationErrors) {
+          this.errorMsgs = err.error.validationErrors;
+        } else {
+          this.errorMsgs.push(err.error.error)
+        }
+      }
+    })
 
   }
 
   register() {
-    
+    this.router.navigate(['register']);
+
   }
 }
